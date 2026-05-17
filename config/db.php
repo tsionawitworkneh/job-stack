6c33851880fcbf51c2f53f6f0452eac403943446
tsionawit <your_email@example.com>
2026-05-18 00:00:41 +0300

    feat: config the database conn

---
+++ config/db.php
@@ -0,0 +1,14 @@
+<?php
+$host = 'localhost';
+$dbname = 'job_stack';
+$username = 'root';
+$password = ''; 
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected to the database successfully!";
+} catch (PDOException $e) {
+    die("Could not connect to the database: " . $e->getMessage());
+}
+?>
