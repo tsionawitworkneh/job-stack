72e667eb843737a4e5b9fbd8a5767272c66d4cd4
tsionawit <your_email@example.com>
2026-05-18 15:56:38 +0300

    feat: add dedicated env for admin login

---
+++ admin/admin_login.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../assets/css/login.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+    <title>Job Stack- Admin Signin</title>   
+</head>
+<body>
+    <section class="signin-section">
+
+    <div class="signin-card">
+
+        
+        
+
+        
+
+        
+        <div class="signin-header">
+
+            <h2>Admin Portal</h2>
+
+            <p>
+                Enter your credentials to access your account
+            </p>
+
+        </div>
+
+        
+        <form class="signin-form">
+
+            <div class="input-group">
+
+                <label>Email Address</label>
+
+                <input 
+                    type="email"
+                    placeholder="name@example.com"
+                    required
+                >
+
+            </div>
+
+            <div class="input-group">
+
+                <div class="password-label">
+
+                    <label>Password</label>
+
+                    
+                </div>
+
+                <input 
+                    type="password"
+                    placeholder="••••••••"
+                    required
+                >
+
+            </div>
+
+            <button type="submit" class="signin-btn">
+                Sign In
+            </button>
+
+        </form>
+
+    
+
+    </div>
+
+</section>
+</body>
+</html>
